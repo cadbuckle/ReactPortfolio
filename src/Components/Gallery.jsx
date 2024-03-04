76ba216a010f793781f3f69df3de9626a44fe612
Duncan B <duncanbuckle@gmail.com>
2024-03-04 09:13:23 +0000

    Base page and components

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Gallery() {
+    return (
+        <h2>This is the Gallery</h2>
+    );
+}
+
+export default Gallery;
---
+++ src/Components/Project.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Project() {
+    return (
+        <h2>This is the project</h2>
+    );
+}
+
+export default Project;
