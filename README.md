2737cc05fe4f0df4851c63be1db836e9654f3ff4
Duncan B <duncanbuckle@gmail.com>
2024-03-05 19:40:47 +0000

    Updated readme and the styling on contacts

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
-# React + Vite
+# REACT Created Portfolio
+![GitHub License](https://img.shields.io/github/license/cadbuckle/ReactPortfolio)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Description
+This is my portolio of code work now built with REACT.
 
-Currently, two official plugins are available:
+## Table of Contents
+* [Links](#links)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Screenshots](#screenshots)
+* [Project Items](#project-items)
+* [License](#license)
+* [Features](#features)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Links
+* [Repo](https://github.com/cadbuckle/ReactPortfolio)
+
+## Installation
+If not already installed, please install node.
+From the root of the app, please run "npm i" to install any additional required packages.
+
+## Usage
+From a bash prompt you mcan run npm run dev.  This will then provide you with a link you can open in your browser which will display the Portfolio page.
+There are 3 tab options:
+* Home - Display details about myself
+* Projects - The top 6 projects I've worked on
+* Contact - How you can contact me.
+
+## Screenshots
+### Home page
+<img src="./assets/Screenshot_HomePage.png" alt="App home page" width=50%></img>
+### Projects page
+<img alt="app projects page" src="./assets/Screenshot_Projects.png" width=50%></img>
+### Contacts page
+<img alt="app contact page" src="./assets/Screenshot_Contact.png" width=50%></img>
+
+## License
+MIT License
+
+## Features
+JavaScript and REACT
+* JavaScript
+* React
+* React Router for navigation
+* Components
+
+## Contributions
+Please email me with details if you wish to contribute further to this app
+
+## Acknowledgements
+Thanks to:
+* Bootcamp Lessons
+
+## Questions?
+My github profile can be found at https://github.com/cadbuckle
+If you wish to contact me, you can email me at duncan.buckle@gmail.com
+
+© 2024 Duncan B. Confidential and Proprietary. All Rights Reserved.
--- src/Components/Contact/ContactFooter.jsx
+++ src/Components/Contact/ContactFooter.jsx
@@ -1,4 +1,4 @@
-import './Contact.css';
+import "./Contact.css";
 
 // Must have contact informatoin
 // Must have a "contact" form with associated events
@@ -6,32 +6,57 @@ function ContactFooter() {
   return (
     <div className="container">
       <div className="row">
-      <div className="col-sm-12 col-md-6 col-lg-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Email</h5>
-            <p className="card-text">
-              <a href="mailto:duncan.buckle@gmail.com">Duncan</a>
-            </p>
+        <div className="col-sm-12 col-md-6 col-lg-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Email</h5>
+              <p className="card-text">
+                <a href="mailto:duncan.buckle@gmail.com">Duncan</a>
+              </p>
+            </div>
           </div>
-        </div></div><div className="col-sm-12 col-md-6 col-lg-4 pb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Call</h5>
-            <p className="card-text">
-              <a href="tel:+441928392814">Duncan</a>
-            </p>
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-4 pb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">GitHub</h5>
+              <p className="card-text">
+                <a href="https://www.github.com/cadbuckle" _target="GitHub">
+                  My GitHub
+                </a>
+              </p>
+            </div>
           </div>
-        </div></div><div className="col-sm-12 col-md-6 col-lg-4 pb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">LinkedIn</h5>
-            <p className="card-text">
-              <a href="https://www.linkedin.com/in/duncanbuckle/" _target="LinkedIn">Duncan Buckle</a>
-            </p>
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-4 pb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">LinkedIn</h5>
+              <p className="card-text">
+                <a
+                  href="https://www.linkedin.com/in/duncanbuckle/"
+                  _target="LinkedIn"
+                >
+                  My LinkedIn
+                </a>
+              </p>
+            </div>
           </div>
-        </div></div>
-
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-4 pb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Resume</h5>
+              <p className="card-text">
+                <a
+                  href="https://drive.google.com/file/d/1sMZfAt0SHZbZ6oFIZGBLb6heQiermV5-/view?usp=sharing" _target="Resume"
+                >
+                  My Resume
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
